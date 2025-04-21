138ae1f76de80df149ff356174dab9d580bc6cd2
Andres <pandybaez@gmail.com>
2025-04-21 15:05:50 -0500

    Cambio1

--- udla/progra1/1.c
+++ udla/progra1/1.c
@@ -3,6 +3,7 @@
 
 int main(){
     char nombres[5][20];
+    int tralaleritos[3][3];
 
     for(int i=0; i<5; i++){
         printf("Escribe el nombre:");
@@ -12,6 +13,9 @@ int main(){
     for(int j=0; j<5; j++){
         printf("%s", nombres[j]);
     }
+
+    int hola=0;
+    printf("sos un capo");
 
     return 0;
 }
